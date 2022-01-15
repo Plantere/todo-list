eeb752a7a1514c3da12544a4461e1712ffd43f75
Fabio Torres <bfabiobenfica@gmail.com>
2022-01-14 23:50:16 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to todoist
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/todoist/core_test.clj
@@ -0,0 +1,7 @@
+(ns todoist.core-test
+  (:require [clojure.test :refer :all]
+            [todoist.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
