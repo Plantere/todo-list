dc7c0b2036564e7a8d20d1350f9a7bd7f1f3da87
Fabio Torres <bfabiobenfica@gmail.com>
2022-01-15 22:27:43 -0300

    added: importing dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [ring "1.9.4"]
+                 [ring/ring-json "0.5.1"]
+                 [compojure "1.6.2"]]
   :main ^:skip-aot todoist.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
