9938cd4798e80c76a7ea21333a40ce215bed8f9a
Fabio Torres <bfabiobenfica@gmail.com>
2022-01-20 23:12:34 -0300

    refactor: variable to manage users table

--- src/todoist/repository/users.clj
+++ src/todoist/repository/users.clj
@@ -1,41 +1,43 @@
 (ns todoist.repository.users
-  (:require [todoist.helpers.database :refer [generate-uuid append-json check-user-exist delete-by get-by edit-by read-json-file]]
+  (:require [todoist.helpers.database :refer [generate-uuid append-json check-exist delete-by get-by edit-by read-json-file]]
             [clojure.java.io :refer :all]))
-            
+
+
+(def table-users "database/users.json")
 
 (defn create
   ([name]
    (let [params {:id (generate-uuid) :name name}]
-     (if (not= (check-user-exist name "name") true)
-       (append-json params "database/users.json")
+     (if (not= (check-exist name "name" table-users) true)
+       (append-json params table-users)
        (throw (AssertionError. "Name is already being used on the system")))))
   ([] (throw (AssertionError. "name is a mandatory parameter"))))
 
 
 (defn delete
   ([id]
-   (if (= (check-user-exist id "id") true)
-     (delete-by id "id" "database/users.json")
+   (if (= (check-exist id "id" table-users) true)
+     (delete-by id "id" table-users)
      (throw (AssertionError. "User not exist in the system"))))
   ([] (throw (AssertionError. "id is a mandatory parameter")))
   )
 
 (defn get-specific
   ([id]
-   (if (= (check-user-exist id "id") true)
-     (get-by id "id" "database/users.json")
+   (if (= (check-exist id "id" table-users) true)
+     (get-by id "id" table-users)
      (throw (AssertionError. "User not exist in the system"))))
   ([] (throw (AssertionError. "id is a mandatory parameter")))
 )
 
 (defn edit
   ([id name]
-   (if (and (= (check-user-exist id "id") true) (not= (check-user-exist name "name") true))
-     (edit-by id "id" "database/users.json" {:id id :name name})
+   (if (and (= (check-exist id "id" table-users) true) (not= (check-exist name "name" table-users) true))
+     (edit-by id "id" table-users {:id id :name name})
      (throw (AssertionError. "User does not exist, or name already being used in the system"))))
    ([] (throw (AssertionError. "id and name is a mandatory parameter"))))
 
 (defn get-all
   []
-  (read-json-file "database/users.json")
+  (read-json-file table-users)
   )
