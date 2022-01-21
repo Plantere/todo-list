8070d7cf1463a99ad024cf4025c22e15f45c8035
Fabio Torres <bfabiobenfica@gmail.com>
2022-01-20 23:13:30 -0300

    refactor: parameter file to table

--- src/todoist/helpers/database.clj
+++ src/todoist/helpers/database.clj
@@ -3,34 +3,34 @@
 
 (defn generate-uuid [] (.toString (java.util.UUID/randomUUID)))
 ;;(defn check-task-exist [name] false)
-(defn read-json-file [file]
-  (json/read-str (slurp file)))
+(defn read-json-file [table]
+  (json/read-str (slurp table)))
 
-(defn check-user-exist [data, type] (some (fn [x] (= (get x type) data)) (read-json-file "database/users.json")))
-(defn conj-and-to-json [params file]
-  (let [users-conj (conj (read-json-file file) params)]
+(defn check-exist [data type table] (some (fn [x] (= (get x type) data)) (read-json-file table)))
+(defn conj-and-to-json [params table]
+  (let [users-conj (conj (read-json-file table) params)]
     (-> users-conj (json/write-str :escape-unicode false))))
     
-(defn append-json [params file]
-  (let [params-converted (conj-and-to-json params file)]
-    (with-open [wrtr (writer file)] (.write wrtr params-converted))))
+(defn append-json [params table]
+  (let [params-converted (conj-and-to-json params table)]
+    (with-open [wrtr (writer table)] (.write wrtr params-converted))))
   
 
-(defn delete-by [data type file]
-  (let [data-filtered (json/write-str (filter (fn [x] (not= (get x type) data)) (read-json-file file)) :escape-unicode false)]
-    (with-open [wrtr (writer file)] (.write wrtr data-filtered))))
+(defn delete-by [data type table]
+  (let [data-filtered (json/write-str (filter (fn [x] (not= (get x type) data)) (read-json-file table)) :escape-unicode false)]
+    (with-open [wrtr (writer table)] (.write wrtr data-filtered))))
     
-(defn get-by [data type file]
-  (let [data-filtered  (filter (fn [x] (= (get x type) data)) (read-json-file file)) ] data-filtered))
+(defn get-by [data type table]
+  (let [data-filtered  (filter (fn [x] (= (get x type) data)) (read-json-file table)) ] data-filtered))
   
 (defn get-position
   [pred coll]
   (keep-indexed (fn [idx x] (when (pred x) idx)) coll))
 
 (defn edit-by
-  [id type file params]
-  (let [json-file (read-json-file file)
+  [id type table params]
+  (let [json-file (read-json-file table)
         index-id (first (get-position (fn [x] (= (get x type) id)) json-file))]
-    (with-open [wrtr (writer file)] (.write wrtr (json/write-str (assoc json-file index-id params) :escape-unicode false)))
+    (with-open [wrtr (writer table)] (.write wrtr (json/write-str (assoc json-file index-id params) :escape-unicode false)))
     )
   )
