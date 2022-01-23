442db310a78504073a10182469e7dbc9cf921546
Fabio Torres <bfabiobenfica@gmail.com>
2022-01-22 22:42:49 -0300

    added: configurations of jetty, and routes

--- src/todoist/core.clj
+++ src/todoist/core.clj
@@ -1,7 +1,23 @@
 (ns todoist.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer [defroutes]]
+            [compojure.route :refer [not-found]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [todoist.routes :as routes]
+            )
   (:gen-class))
 
+(defroutes routes
+  routes/users
+  routes/tasks
+  (not-found
+   {:error "Not found"}))
+
+(def app
+  (-> routes
+      (wrap-json-body {:keywords? true :bigdecimals? true})
+      wrap-json-response))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (run-jetty app {:port 3000}))
